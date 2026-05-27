7db04e631350b6e64732f42cc8c8213717bee10f
jguo <jichen.guo@dfki.de>
2026-05-27 10:51:04 +0200

    Add new checkpoint path and image paths

--- examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/detect_video.sh
+++ examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/detect_video.sh
@@ -5,19 +5,20 @@
 ###############################################################################
 
 set -e
+export RFDETR_JIT_GRAD=0
 
 SIF=~/images/paz_fish_jax.sif
 
 SCRIPT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/detect_video.py
 
-VIDEO=~/datasets/input_video.mp4
+VIDEO=~/videos/Trim_1.mp4
 
-CHECKPOINT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_runs/crab_lobster_oversampled/rfdetr_nano_finetuned_final.weights.h5
+CHECKPOINT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_oversampled_epoch100/checkpoint_best_total.weights.h5
 
-OUTPUT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_results/detected_video.mp4
-JSON_OUTPUT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_results/detected_video.json
+OUTPUT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_results/detected_video_1.mp4
+JSON_OUTPUT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_results/detected_video_1.json
 
-THRESHOLD=0.8
+THRESHOLD=0.5
 
 singularity exec --nv \
     --bind /mnt/beegfs:/mnt/beegfs \
