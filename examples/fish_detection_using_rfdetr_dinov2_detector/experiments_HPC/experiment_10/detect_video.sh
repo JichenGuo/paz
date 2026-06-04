d8c66a6cea09f702a6ed23217617adc6b9345d3d
jguo <jichen.guo@dfki.de>
2026-06-04 18:32:10 +0200

    Add new path for videos and checkpoints

--- examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/detect_video.sh
+++ examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/detect_video.sh
@@ -11,12 +11,12 @@ SIF=~/images/paz_fish_jax.sif
 
 SCRIPT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/detect_video.py
 
-VIDEO=~/videos/Trim_1.mp4
+VIDEO=~/videos/Trim_2.mp4
 
-CHECKPOINT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_oversampled_epoch100/checkpoint_best_total.weights.h5
+CHECKPOINT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_oversample_epoch100_v2/checkpoint_best_total.weights.h5
+OUTPUT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_results/detected_video_2_v2_nanov2.mp4
+JSON_OUTPUT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_results/detected_video_2_v2_nanov2.json
 
-OUTPUT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_results/detected_video_1_v2.mp4
-JSON_OUTPUT=/mnt/beegfs/home/jguo/projects/fish_detector_using_rfdetr/paz/examples/fish_detection_using_rfdetr_dinov2_detector/experiments_HPC/experiment_10/finetune_results/detected_video_1_v2.json
 
 THRESHOLD=0.5
 
